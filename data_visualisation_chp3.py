fdab2561c2797ce3afaffdbcb2eabf9309f590ea
dadalib <darwin.libioulle@gmail.com>
2022-06-28 16:28:09 +0200

    Setup file

--- data_visualisation_chp3.py
+++ data_visualisation_chp3.py
@@ -1,4 +1,17 @@
 # Add comments
-from matplotlib import pyplot as pyplot
+import sys
+
+print("Version",sys.version)
+print("FILE",sys.executable)
+import matplotlib
+from matplotlib import pyplot as plt
 years = [1950,1960,1970,1980,2000,2010]
 gdp = [300.2,543.3,1075.3,2862.5,5979.6,10289.7,14958.3]
+
+# Graph with x as years and y as PIB
+plt.plot(years,gdp, color='green',marker='o',linestyle='solid')
+# Add a tittle
+plt.tittle("Valeurs de GDP")
+# Add label in y
+plt.ylabel("Millard des dollars")
+plt.show()
